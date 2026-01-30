19705bcf18d0d21c6ad78f0ae9a4bfc86760f844
olier02 <149459518+olier02@users.noreply.github.com>
2026-01-30 08:02:02 +0100

    Add files via upload
    reste la console pour character

---
+++ abs true.cpp
@@ -0,0 +1,22 @@
+#include <cinttypes>
+#include <iostream>
+
+int main()
+{
+do
+{
+	int32_t (x);
+    printf( "Veuillez saisir un entier : " );
+    scanf( "%d", &x );
+
+  abs(x);
+  printf( "La valeur absolue de %d vaut %d.\n",x,abs(x));  
+}
+
+while (abs);
+
+}
+
+
+
+ 
